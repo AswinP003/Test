d54d8947fc2c8df52df097ad9bf44ae1a2aca096
AswinP003 <106226783+AswinP003@users.noreply.github.com>
2022-05-25 11:59:08 +0530

    Create Calc.py
    Python program for a simple calculator

---
+++ Calc.py
@@ -0,0 +1,12 @@
+print("A simple calculator")
+a=int(input("Enter the first number:))
+b=intn(input("Enter the second number:"))
+op=input("Enter the operation to perform: ")
+if(op== "+"):
+ print(a+b)
+elif(op== "-"):
+ print(a-b)
+elif(op== "*"):
+ print(a*b)
+elif(op== "/"):
+ print(a/b)
